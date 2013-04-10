c9531561ce89498cecce35e9e83dc8baddd55281
Karl Woditsch <k.woditsch@gmail.com>
2013-04-10 20:20:50 +0200

    Base parameters gui

--- MacProtectionGui/src/MacProtectionController.java
+++ MacProtectionGui/src/MacProtectionController.java
@@ -9,7 +9,12 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TreeView;
 
 /**
  *
@@ -18,12 +23,20 @@ import javafx.scene.control.Label;
 public class MacProtectionController implements Initializable {
     
     @FXML
-    private Label label;
-    
-    @FXML
     private void handleQuitAction(ActionEvent event) {
         Platform.exit();
     }
+    
+    @FXML
+    private ChoiceBox choiceAlgorithm;
+    @FXML
+    private PasswordField choicePassword;
+    @FXML
+    private Button choiceRoot;
+    @FXML
+    private TextField rootView;
+    @FXML
+    private TreeView treeView;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
