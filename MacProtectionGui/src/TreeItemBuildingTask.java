ac829500eecf4d67b03dd3c4671329c14a19a94c
Karl Woditsch <k.woditsch@gmail.com>
2013-04-10 23:42:10 +0200

    Add progress javafx property

--- MacProtectionGui/src/TreeItemBuildingTask.java
+++ MacProtectionGui/src/TreeItemBuildingTask.java
@@ -1,5 +1,8 @@
 
+import core.processor.MacProcessor;
 import core.tree.Folder;
+import javafx.beans.property.ReadOnlyDoubleProperty;
+import javafx.beans.property.ReadOnlyDoubleWrapper;
 import javafx.concurrent.Task;
 import javafx.scene.control.TreeItem;
 
@@ -14,7 +17,20 @@ import javafx.scene.control.TreeItem;
  * @author Karl
  */
 public class TreeItemBuildingTask extends Task{
+    private ReadOnlyDoubleWrapper progress = new ReadOnlyDoubleWrapper(0d);
+    private int totalNodes;
+    private int processedNodes;
+    
     private Folder rootFolder;
+    
+    /**
+     * Return the ProgressProperty of this Task.
+     * 
+     * @return The ProgressProperty of this Task.
+     */
+    public ReadOnlyDoubleProperty processProgressProperty(){
+        return this.progress.getReadOnlyProperty();
+    }
 
     /**
      * Build a {@link TreeItem} tree from a {@link Folder} tree.
@@ -23,6 +39,8 @@ public class TreeItemBuildingTask extends Task{
      */
     public TreeItemBuildingTask(Folder rootFolder) {
         this.rootFolder = rootFolder;
+        this.totalNodes = rootFolder.getAllSubFolders().size();
+        this.processedNodes = 0;
     }
 
     /**
@@ -37,6 +55,7 @@ public class TreeItemBuildingTask extends Task{
         for(Folder f : folder.getSubFolders()){
             node.getChildren().add(this.buildFolderTreeItem(f));
         }
+        this.progress.set((double) this.processedNodes++ / (double) this.totalNodes);
         return node;
     }
     
