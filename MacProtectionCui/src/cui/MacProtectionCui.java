e9b1c26127959823c05d10ee32e475d39fcce05b
Olivarès Georges <olivares.georges@gmail.com>
2013-04-18 14:43:09 +0200

    add JSAP library
    and arguments support

--- MacProtectionCui/src/cui/MacProtectionCui.java
+++ MacProtectionCui/src/cui/MacProtectionCui.java
@@ -1,10 +1,21 @@
 package cui;
 
+import com.martiansoftware.jsap.JSAP;
+import com.martiansoftware.jsap.JSAPException;
+import com.martiansoftware.jsap.JSAPResult;
+import cui.command.MacProtectionCommandInit;
+import cui.command.MacProtectionCommand;
+import java.util.TreeMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Georges OLIVARES <dev@olivares-georges.fr>
  */
 public class MacProtectionCui {
+
+    protected TreeMap<String, MacProtectionCommand> commands = new TreeMap<>();
 
     /**
      * @param args the command line arguments
@@ -14,14 +25,40 @@ public class MacProtectionCui {
     }
 
     MacProtectionCui() {
-        // @TODO create pseudo console
+        commands.put("init", new MacProtectionCommandInit());
     }
 
-    public static void callFactory(String string, String[] args) {
-        System.out.println("call " + string);
-        System.out.println("-- with args : ");
-        for (String arg : args) {
-            System.out.println("\t" + arg);
+    public static void callFactory(String string, String[] args) throws CuiMessageException {
+
+        MacProtectionCui MPC = new MacProtectionCui();
+
+        if (!MPC.getCommands().containsKey(string)) {
+            throw new CuiMessageException("Unknown command");
         }
+
+        try {
+            MacProtectionCommand command = MPC.getCommands().get(string);
+            JSAP jsap;
+            jsap = command.initCall();
+            JSAPResult config = jsap.parse(args);
+
+            // check whether the command line was valid, and if it wasn't,
+            // display usage information and exit.
+            if (!config.success()) {
+                System.err.println();
+                System.err.println("Usage: java " + command.getClass().getName());
+                System.err.println("\t" + jsap.getUsage());
+                System.err.println();
+                System.exit(1);
+            }
+
+            command.process(config);
+        } catch (JSAPException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    public TreeMap<String, MacProtectionCommand> getCommands() {
+        return this.commands;
     }
 }
---
+++ MacProtectionCui/src/cui/command/MacProtectionCommand.java
@@ -0,0 +1,16 @@
+package cui.command;
+
+import com.martiansoftware.jsap.JSAP;
+import com.martiansoftware.jsap.JSAPException;
+import com.martiansoftware.jsap.JSAPResult;
+
+/**
+ *
+ * @author Georges OLIVARES <dev@olivares-georges.fr>
+ */
+public interface MacProtectionCommand {
+
+    public JSAP initCall() throws JSAPException;
+
+    public void process(JSAPResult config);
+}
