adf2692857d28e8f1327ef4754e1f72a9a08a6de
Olivarès Georges <olivares.georges@gmail.com>
2013-04-18 13:22:21 +0200

    CUi : first commit

---
+++ MacProtectionCui/src/macprotectioncui/MacProtectionCui.java
@@ -0,0 +1,18 @@
+/*
+ * 
+ */
+package macprotectioncui;
+
+/**
+ *
+ * @author Georges OLIVARES <dev@olivares-georges.fr>
+ */
+public class MacProtectionCui {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
