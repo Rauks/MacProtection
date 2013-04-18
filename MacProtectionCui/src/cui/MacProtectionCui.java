1d9bccada40a1047c30020c630a4b1dba1e8f21d
Olivarès Georges <olivares.georges@gmail.com>
2013-04-18 14:05:13 +0200

    CUi : fix static main

--- MacProtectionCui/src/cui/MacProtectionCui.java
+++ MacProtectionCui/src/cui/MacProtectionCui.java
@@ -10,7 +10,7 @@ public class MacProtectionCui {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        MacProtectionCui();
+        new MacProtectionCui();
     }
 
     MacProtectionCui() {
