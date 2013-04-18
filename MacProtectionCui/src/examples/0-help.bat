51f4f40b052308ccbeb7c8542324ccfe7064a79a
Olivarès Georges <olivares.georges@gmail.com>
2013-04-18 14:58:50 +0200

    CUi : edit .bat

---
+++ MacProtectionCui/src/examples/0-help.bat
@@ -0,0 +1,2 @@
+java -jar ../../dist/MacProtectionCui.jar help
+Pause
--- MacProtectionCui/src/examples/1-init.bat
+++ MacProtectionCui/src/examples/1-init.bat
@@ -1,3 +1,5 @@
+java -jar ../../dist/MacProtectionCui.jar notExists
+
 java -jar ../../dist/MacProtectionCui.jar init fail
 
 java -jar ../../dist/MacProtectionCui.jar init -p password
