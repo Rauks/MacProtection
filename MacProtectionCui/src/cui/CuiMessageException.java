48f55f09e3f3a317fc6a5d0a0186765468a12383
Olivarès Georges <olivares.georges@gmail.com>
2013-04-18 14:57:59 +0200

    CUi : add CuiMessageException(Exception)

--- MacProtectionCui/src/cui/CuiMessageException.java
+++ MacProtectionCui/src/cui/CuiMessageException.java
@@ -1,5 +1,7 @@
 package cui;
 
+import com.martiansoftware.jsap.JSAPException;
+
 /**
  *
  * @author Georges OLIVARES <dev@olivares-georges.fr>
@@ -8,6 +10,10 @@ class CuiMessageException extends Exception {
 
     CuiMessageException(String message) {
         super(message);
+    }
+
+    CuiMessageException(JSAPException ex) {
+        super(ex);
     }
     
 }
