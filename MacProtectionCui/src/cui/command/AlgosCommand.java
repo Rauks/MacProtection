fb8c37dfba69c9433b58a2a6d122326bf41119b4
Olivarès Georges <olivares.georges@gmail.com>
2013-04-25 11:07:02 +0200

    CUi : algo exec code
    list all algos

--- MacProtectionCui/src/cui/command/AlgosCommand.java
+++ MacProtectionCui/src/cui/command/AlgosCommand.java
@@ -3,6 +3,7 @@ package cui.command;
 import com.martiansoftware.jsap.JSAP;
 import com.martiansoftware.jsap.JSAPException;
 import com.martiansoftware.jsap.JSAPResult;
+import core.MacAlgorithm;
 
 /**
  *
@@ -13,12 +14,16 @@ public class AlgosCommand implements MacProtectionCommand {
     @Override
     public JSAP initCall() throws JSAPException {
         JSAP jsap = new JSAP();
-        
+
         return jsap;
     }
 
     @Override
     public void process(JSAPResult config) {
-        
+        //List and choice of the Hmac algorithm.
+        System.out.println("Available algorithms :");
+        for (String alg : MacAlgorithm.AVAILABLE_ALGORITHMS) {
+            System.out.println(" - " + alg);
+        }
     }
 }
