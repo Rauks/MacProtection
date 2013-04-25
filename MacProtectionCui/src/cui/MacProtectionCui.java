711ef59a43fcf55096b40a2fb0688611eeaffe06
Olivarès Georges <olivares.georges@gmail.com>
2013-04-25 21:38:31 +0200

    CUi : init command useless

--- MacProtectionCui/src/cui/MacProtectionCui.java
+++ MacProtectionCui/src/cui/MacProtectionCui.java
@@ -25,7 +25,6 @@ public class MacProtectionCui {
 
     MacProtectionCui() {
         // initialize the commands
-        MacProtectionCui.commands.put("init", new cui.command.InitCommand());
         MacProtectionCui.commands.put("ls", new cui.command.LsCommand());
         MacProtectionCui.commands.put("show", new cui.command.ShowCommand());
         MacProtectionCui.commands.put("diff", new cui.command.DiffCommand());
