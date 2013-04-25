05c93721ff94bb48a3ef365c552a796d9fd4e8f7
Olivarès Georges <olivares.georges@gmail.com>
2013-04-25 20:13:26 +0200

    CUi : MacProtectionActionFactory

---
+++ MacProtectionCui/src/cui/MacProtectionActionsFactory.java
@@ -0,0 +1,67 @@
+package cui;
+
+import core.MacAlgorithm;
+import core.check.CheckMacException;
+import core.check.CheckReader;
+import core.check.CheckReaderReadingException;
+import core.processor.MacProcessor;
+import core.processor.MacProcessorEvent;
+import core.processor.MacProcessorException;
+import core.processor.MacProcessorListener;
+import core.tree.Folder;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ *
+ * @author Georges OLIVARES <dev@olivares-georges.fr>
+ */
+public class MacProtectionActionsFactory {
+
+    public static MacProcessor scanDirectory(File dirToScan, MacAlgorithm algorithm, String opt_password, MacProcessor.MacOutput mcOutput) throws MacProcessorException {
+        System.out.println();
+        
+        MacProcessor p = new MacProcessor(dirToScan, algorithm, opt_password, mcOutput);
+
+        p.addMacProcessorListener(new MacProcessorListener() {
+            private int state_process = 0;
+            private boolean introIsPrinted;
+
+            @Override
+            public void macProcessorPerformed(MacProcessorEvent evt) {
+                float state = evt.getProcessedFiles() / (float) evt.getTotalFiles() * 100;
+
+                if (!this.introIsPrinted) {
+                    System.out.println("Numbers of files : " + evt.getTotalFiles());
+                    System.out.print("\tScan progres : ");
+                    this.introIsPrinted = true;
+                }
+
+                if (Math.floor(state / 2) > this.state_process) {
+                    System.out.print("|");
+                    this.state_process = (int) Math.floor(state / 2);
+                }
+            }
+        });
+        p.process();
+
+        System.out.println();
+
+        return p;
+    }
+
+    public static CheckReader checkReader(String opt_file, MacAlgorithm algorithm, String opt_password) throws FileNotFoundException, CheckReaderReadingException, CheckMacException, NoSuchAlgorithmException, InvalidKeyException {
+        System.out.println();
+        System.out.println("Compare with check file '" + opt_file + "' ... ");
+
+        System.out.print("\tReading ... ");
+        CheckReader cr = new CheckReader(new FileInputStream(new File(opt_file)), algorithm, opt_password);
+        cr.read();
+        System.out.println(" DONE !");
+        
+        return cr;
+    }
+}
