7f47d98a82a790a54d638f41cfa6f6a0cddcfc5f
Olivarès Georges <olivares.georges@gmail.com>
2013-04-25 22:21:16 +0200

    CUi : main

--- MacProtectionCui/src/cui/Main.java
+++ MacProtectionCui/src/cui/Main.java
@@ -1,6 +1,8 @@
 package cui;
 
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -12,8 +14,6 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //String[] args = new String[] {"help"};
-
         try {
             if (args.length < 1) {
                 throw new CuiMessageException("You must specify at least one argument");
@@ -27,8 +27,15 @@ public class Main {
             MacProtectionCui.callFactory(args[0], (String[]) optionsArgs.toArray(new String[optionsArgs.size()]));
 
         } catch (CuiMessageException e) {
-            System.out.println(e);
+            System.out.println(e.getMessage());
+            System.out.println();
             System.out.println("Read the documentation (readme) or use `help` command");
+            System.out.println();
+            try {
+                MacProtectionCui.callFactory("help", new String[] {});
+            } catch (CuiMessageException ex) {
+                System.err.println(ex);
+            }
         }
     }
 }
