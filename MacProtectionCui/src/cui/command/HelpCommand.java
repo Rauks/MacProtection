5d19cc9de14f60a29647638ab20213747fce083c
Olivarès Georges <olivares.georges@gmail.com>
2013-04-25 21:27:01 +0200

    CUi : edit Help command
    new architecture

--- MacProtectionCui/src/cui/command/HelpCommand.java
+++ MacProtectionCui/src/cui/command/HelpCommand.java
@@ -1,8 +1,10 @@
 package cui.command;
 
+import com.martiansoftware.jsap.FlaggedOption;
 import com.martiansoftware.jsap.JSAP;
 import com.martiansoftware.jsap.JSAPException;
 import com.martiansoftware.jsap.JSAPResult;
+import com.martiansoftware.jsap.UnflaggedOption;
 import cui.MacProtectionCui;
 import java.util.Map;
 import java.util.logging.Level;
@@ -17,25 +19,48 @@ public class HelpCommand implements MacProtectionCommand {
     @Override
     public JSAP initCall() throws JSAPException {
         JSAP jsap = new JSAP();
+
+        UnflaggedOption opt1 = new UnflaggedOption("command")
+                .setStringParser(JSAP.STRING_PARSER)
+                .setRequired(false);
+        jsap.registerParameter(opt1);
+
         return jsap;
     }
 
     @Override
     public void process(JSAPResult config) {
+        String opt_command = config.getString("command");
+
         System.out.println("Help");
         System.out.println();
 
-        for (Map.Entry<String, MacProtectionCommand> entry : MacProtectionCui.getCommands().entrySet()) {
+        if (opt_command != null && !opt_command.isEmpty()) {
+            MacProtectionCommand entry = MacProtectionCui.getCommands().get(opt_command);
             try {
-                JSAP jsap = entry.getValue().initCall();
+                JSAP jsap = entry.initCall();
                 System.out.print("   ");
-                System.out.print(entry.getKey());
+                System.out.print(opt_command);
                 System.out.print("\t");
                 System.out.print(jsap.getUsage());
                 System.out.print(implode("\n\t\t", jsap.getHelp().split("\n"), true));
                 System.out.println("\n");
             } catch (JSAPException ex) {
-                Logger.getLogger(HelpCommand.class.getName()).log(Level.SEVERE, null, ex);
+                System.err.println(ex);
+            }
+        } else {
+
+            for (Map.Entry<String, MacProtectionCommand> entry : MacProtectionCui.getCommands().entrySet()) {
+                try {
+                    JSAP jsap = entry.getValue().initCall();
+                    System.out.print("   ");
+                    System.out.print(entry.getKey());
+                    System.out.print("\t");
+                    System.out.print(jsap.getUsage());
+                    System.out.println("\n");
+                } catch (JSAPException ex) {
+                    Logger.getLogger(HelpCommand.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
         }
     }
